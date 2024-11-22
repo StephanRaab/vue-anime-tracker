0d3fb582a51ae5ed36094acae89f5bf227c36ec1
Stephan Raab <sarcartist@gmail.com>
2024-11-22 13:05:05 -0500

    init commit

---
+++ vue-anime-tracker/src/components/TheHome.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1>Vue Anime Tracker</h1>
+
+    <form>
+      <input type="text"  placeholder="search for an anime..."> <button>Search</button>
+    </form>
+
+
+  </section>
+
+  <section>
+    <h2>My Anime</h2>
+    
+    <div class="my-anime-container">
+      <div class="my-anime">
+        <img/>
+        <p>Dragon Ball</p>
+
+        <div>
+          <p>58/153</p>
+          <button class="increment-btn">+</button>
+          <button class="decrement-btn">-</button>  
+        </div>
+        
+      </div>
+    </div>
+
+  </section>
+
+</template>
+
+<style scoped>
+.my-anime {
+  display: inline-flex;
+  justify-content: space-between;
+  width: 80vh;
+  background-color: #3e3e3e;
+  padding: 1.5rem;
+  border-radius: 1rem;
+}
+</style>
---
+++ vue-anime-tracker/src/views/CompletedView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="completed">
+    <h1>This is the completed page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .completed {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
