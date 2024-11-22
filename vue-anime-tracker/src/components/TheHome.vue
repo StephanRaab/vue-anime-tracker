268bd662e51a66f60cd87966080289c2f11ddee6
Stephan Raab <sarcartist@gmail.com>
2024-11-22 17:28:34 -0500

    update buttons to be disabled

--- vue-anime-tracker/src/components/TheHome.vue
+++ vue-anime-tracker/src/components/TheHome.vue
@@ -39,7 +39,7 @@ const addAnime = anime => {
     my_anime.value.push({
       id: anime.mal_id,
       title: anime.title,
-      img: anime.images.jpg.small_image_url,
+      img: anime.images.jpg.image_url,
       watched_episodes: 0,
       total_episodes: anime.episodes,
       date_added: Date.now(),
@@ -69,8 +69,6 @@ const decrementWatchCount = anime => {
   localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
 }
 
-
-// const deleteAnime
 // const completeAnime
 
 onMounted(() => {
@@ -79,51 +77,53 @@ onMounted(() => {
 </script>
 
 <template>
-  <main>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
-    <h1>Vue Anime Tracker</h1>
+  <div>
+    <main>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
+      <h1>Vue Anime Tracker</h1>
 
-    <form @submit.prevent="searchAnime">
-      <input type="text" placeholder="search for an anime..." v-model="query" @input="handleInput" />
-      <button type="submit">Search</button>
-    </form>
-  </main>
+      <form @submit.prevent="searchAnime">
+        <input type="text" placeholder="search for an anime..." v-model="query" @input="handleInput" />
+        <button type="submit">Search</button>
+      </form>
+    </main>
 
-  <div v-if="search_results" class="anime-search-results">
-    <div class="search-result" v-for="anime in search_results" :key="anime.mal_id">
-      <img :src="anime.images.jpg.image_url" />
-      <div class="details">
-        <h3>{{ anime.title }}</h3>
-        <pre :title="anime.synopsis" v-if="anime.synopsis">{{ anime.synopsis }}</pre>
-        <span class="flex-1" />
-        <button @click="addAnime(anime)">Add to My Anime</button>
-      </div>
-    </div>
-  </div>
-
-  <section>
-    <h2>My Anime</h2>
-
-    <div class="my-anime-container">
-      <div v-for="anime in my_anime_asc" :key="anime.id">
-        <img :src="anime.img" />
-        <p>{{ anime.title }}</p>
-
-        <div class="anime-progress">
-          <p>Progress: {{ anime.watched_episodes }}/{{ anime.total_episodes }}</p>
-
-          <button v-if="anime.watched_episodes" class="decrement-btn" @click="decrementWatchCount(anime)">-</button>
-          <button v-if="anime.watched_episodes < anime.total_episodes" class="increment-btn"
-            @click="incrementWatchCount(anime)">+</button>
-
-          <button class="delete-btn" @click="deleteAnime(anime)">Delete</button>
+    <div v-if="search_results" class="anime-search-results">
+      <div class="search-result" v-for="anime in search_results" :key="anime.mal_id">
+        <img :src="anime.images.jpg.image_url" />
+        <div class="details">
+          <h3>{{ anime.title }}</h3>
+          <pre :title="anime.synopsis" v-if="anime.synopsis">{{ anime.synopsis }}</pre>
+          <span class="flex-1" />
+          <button @click="addAnime(anime)">Add to My Anime</button>
         </div>
-
       </div>
     </div>
 
-  </section>
+    <section>
+      <h2>My Anime</h2>
 
+      <div class="my-anime-container">
+        <div v-for="anime in my_anime_asc" :key="anime.id">
+          <img :src="anime.img" />
+          <h3>{{ anime.title }}</h3>
+
+          <div class="flex-1"></div>
+
+          <div class="anime-progress">
+            <span>Progress: {{ anime.watched_episodes }}/{{ anime.total_episodes }}</span>
+
+            <button :disabled="anime.watched_episodes == 0" class="decrement-btn" @click="decrementWatchCount(anime)">-</button>
+            <button :disabled="anime.watched_episodes == anime.total_episodes" class="increment-btn"
+              @click="incrementWatchCount(anime)">+</button>
+
+            <button class="delete-btn" @click="deleteAnime(anime)">Delete</button>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
@@ -134,5 +134,19 @@ onMounted(() => {
   background-color: #3e3e3e;
   padding: 1.5rem;
   border-radius: 1rem;
+}
+
+.anime-search-results {
+  max-height: 70vh;
+  width: 60vw;
+  overflow: scroll;
+  margin: 1.2em;
+  background-color: gray;
+  padding: 1.2rem;
+}
+
+.search-result {
+  display: flex;
+  flex-direction: column;
 }
 </style>
